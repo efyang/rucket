8b68122aab5ca6f141a9f88969db8bcedbacb7b9
Edward Yang <edward.yang6771@gmail.com>
2016-01-07 17:27:44 -0600

    Finish library generator

--- build.rs
+++ build.rs
@@ -1,43 +1,55 @@
 use std::path::Path;
-use std::fs::copy;
+use std::fs::{File, copy};
 use std::env;
-use std::io::{File, Read};
+use std::io::{Read, Write};
 
 const RKT_FNAME: &'static str = "rust_functions.rkt";
 const COUNTRY_DATA: &'static str = "countrydata.txt";
-const LIB_BEGIN: &'static str = "
 
-";
-
-macro_rules! generate_countries {
-    ($filepath:expr) => {{
-        let country_file = File::open($filepath).unwrap();
-        let country_data = 
-        let mut countries = Vec::new();
-        let mut current_country = Country::new("");
-        for line in data.split("\n") {
-            if line != "" {
-                let vars = line.split(" ")
-                               .map(|s| s.to_string())
-                               .collect::<Vec<String>>();
-                if vars[0].parse::<isize>().is_err() {
-                    // country name, add previous country to countries
-                    countries.push(current_country);
-                    current_country = Country::new(line);
-                } else {
-                    // hitbox for current country
-                    let ireps = vars.iter()
-                                    .map(|s| s.parse::<isize>().unwrap())
-                                    .collect::<Vec<isize>>();
-                    let hb = Hitbox::new(ireps[0], ireps[1], ireps[2], ireps[3]);
-                    current_country.add_hitbox(hb);
-                }
+fn parse_countries(data: &str) -> Vec<Country> {
+    let mut countries = Vec::new();
+    let mut current_country = Country::new("");
+    for line in data.split("\n") {
+        if line != "" {
+            let vars = line.split(" ")
+                           .map(|s| s.to_string())
+                           .collect::<Vec<String>>();
+            if vars[0].parse::<isize>().is_err() {
+                // country name, add previous country to countries
+                countries.push(current_country);
+                current_country = Country::new(line);
+            } else {
+                // hitbox for current country
+                let ireps = vars.iter()
+                                .map(|s| s.parse::<isize>().unwrap())
+                                .collect::<Vec<isize>>();
+                let hb = Hitbox::new(ireps[0], ireps[1], ireps[2], ireps[3]);
+                current_country.add_hitbox(hb);
             }
         }
-        countries.push(current_country);
-        countries.remove(0);
-        countries
-    }}
+    }
+    countries.push(current_country);
+    countries.remove(0);
+    countries
+}
+
+fn generate_declaration(country: &Country) -> String {
+    ["Country { \n    name: \"",
+     &country.name as &str,
+     "\",",
+     "\n    hitboxes: &",
+     &format!("{:#?}", &country.hitboxes) as &str,
+     "\n}"]
+        .join("")
+}
+
+fn generate_declarations(country_data: &[Country]) -> String {
+    let mut countrydecs = "// countries".to_string();
+    for country in country_data.iter() {
+        countrydecs = [&countrydecs as &str, &generate_declaration(country) as &str].join(",\n");
+    }
+    ["static PARSED_COUNTRIES: &'static [Country<'static>] = &[", &countrydecs as &str, "];\n"]
+        .join("\n")
 }
 
 fn main() {
@@ -51,20 +63,94 @@ fn main() {
     let rkt_out_path = target_dir.join(RKT_FNAME);
     copy(rkt_src_path, rkt_out_path).expect(&format!("Failed to copy {}", RKT_FNAME));
     let country_data_path = src_dir.join(COUNTRY_DATA);
+    let mut country_data_file = File::open(country_data_path)
+                                    .expect("Failed to open country data file to read.");
+    let mut country_data_raw = String::new();
+    country_data_file.read_to_string(&mut country_data_raw)
+                     .expect("Failed to read raw country data.");
+    let country_data = parse_countries(&country_data_raw);
     let main_lib_path = src_dir.join("lib.rs");
+    let mut main_lib_file = File::create(main_lib_path)
+                                .expect("Failed to open main lib file to write.");
+    main_lib_file.write_all([LIB_NOGEN, &generate_declarations(&country_data) as &str]
+                                .join("\n")
+                                .as_bytes())
+                 .expect("Failed to write genned code.");
+}
 
+const LIB_NOGEN: &'static str = "extern crate libc;
+
+use libc::c_int;
+use std::ffi::CString;
+
+\
+                                 #[no_mangle]
+pub extern \"C\" fn print_countries() {
+    \
+                                 println!(\"{:#?}\", PARSED_COUNTRIES);
+}
+
+#[no_mangle]
+pub \
+                                 extern \"C\" fn get_country(mousex: c_int, mousey: c_int) -> \
+                                 CString {
+    let mousepoint = Point::new(mousex as isize, \
+                                 mousey as isize);
+    for country in PARSED_COUNTRIES.iter() {
+        \
+                                 for hitbox in country.hitboxes.iter() {
+            if \
+                                 hitbox.point_within(&mousepoint) {
+                return \
+                                 CString::new(country.name.clone()).unwrap();
+            }
+        \
+                                 }
+    }
+    CString::new(\"No Country Found\").unwrap()
+}
+
+impl \
+                                 Point {
+    fn new(x: isize, y: isize) -> Point {
+        Point \
+                                 { x: x, y: y }
+    }
 }
 
 struct Point {
     pub x: isize,
-    pub y: isize,
+    pub \
+                                 y: isize,
 }
 
-impl Point {
-    fn new(x: isize, y: isize) -> Point {
-        Point { x: x, y: y }
+#[derive(Debug)]
+struct Hitbox {
+    xmin: isize,
+    \
+                                 xmax: isize,
+    ymin: isize,
+    ymax: isize,
+}
+
+impl Hitbox {
+    \
+                                 fn point_within(&self, point: &Point) -> bool {
+        (point.x \
+                                 >= self.xmin) && (point.x <= self.xmax) && (point.y >= \
+                                 self.ymin) &&
+        (point.y <= self.ymax)
     }
 }
+
+\
+                                 #[derive(Debug)]
+struct Country<'a> {
+    pub name: &'a str,
+    \
+                                 pub hitboxes: &'a [Hitbox],
+}
+";
 
 #[derive(Debug)]
 struct Hitbox {
@@ -82,11 +168,6 @@ impl Hitbox {
             ymin: ymin,
             ymax: ymax,
         }
-    }
-
-    fn point_within(&self, point: &Point) -> bool {
-        (point.x >= self.xmin) && (point.x <= self.xmax) && (point.y >= self.ymin) &&
-        (point.y <= self.ymax)
     }
 }
 
@@ -108,5 +189,3 @@ impl Country {
         self.hitboxes.push(hb);
     }
 }
-
-
