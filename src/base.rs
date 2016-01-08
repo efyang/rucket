ff3026bc50e27840e032dad05d2000806fa18800
Edward Yang <edward.yang6771@gmail.com>
2016-01-08 04:30:06 -0600

    Add simd and padding

--- src/base.rs
+++ src/base.rs
@@ -1,44 +1,82 @@
 extern crate libc;
+extern crate simd;
 
 use libc::c_int;
 use std::ffi::CString;
+use simd::i32x4;
+
+const BOARDW: i32 = 1307;
+const BOARDH: i32 = 875;
 
 #[no_mangle]
-pub extern "C" fn get_country(mousex: c_int, mousey: c_int) -> CString {
-    let mousepoint = Point::new(mousex as isize, mousey as isize);
+pub extern "C" fn get_x_pad(scrw: c_int) -> c_int {
+    (scrw - BOARDW)/2
+}
+
+#[no_mangle]
+pub extern "C" fn get_y_pad(scrh: c_int) -> c_int {
+    (scrh - BOARDH)/2
+}
+
+#[no_mangle]
+pub extern "C" fn get_country(mousex: c_int, mousey: c_int, xpad: c_int, ypad: c_int) -> CString {
+    let mousepoint = Point::new(mousex, mousey);
+    // board hitbox
+    // make this math block run once somehow?
+    let boardbox = Hitbox {
+        xmin: 0,
+        xmax: BOARDW,
+        ymin: 0,
+        ymax: BOARDH,
+    };
+    if !boardbox.point_within(&mousepoint, xpad, ypad) {
+        // mouse not in board at all
+        return CString::new("null").unwrap();
+    }
+    drop(xpad);
+    drop(ypad);
+    drop(boardbox);
     for country in PARSED_COUNTRIES.iter() {
         for hitbox in country.hitboxes.iter() {
-            if hitbox.point_within(&mousepoint) {
+            if hitbox.point_within(&mousepoint, xpad, ypad) {
                 return CString::new(country.name.clone()).unwrap();
             }
-
         }
     }
-    CString::new("No Country Found").unwrap()
+    CString::new("null").unwrap()
 }
 
 impl Point {
-    fn new(x: isize, y: isize) -> Point {
+    fn new(x: i32, y: i32) -> Point {
         Point { x: x, y: y }
     }
 }
 
 struct Point {
-    pub x: isize,
-    pub y: isize,
+    pub x: i32,
+    pub y: i32,
 }
 
 struct Hitbox {
-    xmin: isize,
-    xmax: isize,
-    ymin: isize,
-    ymax: isize,
+    xmin: i32,
+    xmax: i32,
+    ymin: i32,
+    ymax: i32,
 }
 
 impl Hitbox {
-    fn point_within(&self, point: &Point) -> bool {
-        (point.x >= self.xmin) && (point.x <= self.xmax) && (point.y >= self.ymin) &&
-        (point.y <= self.ymax)
+    fn point_within(&self, point: &Point, xpad: i32, ypad: i32) -> bool {
+        // simd for padding
+        let hitboxvec = i32x4::new(self.xmin,
+                                   self.xmax,
+                                   self.ymin,
+                                   self.ymax);
+        let padvec = i32x4::new(xpad, xpad, ypad, ypad);
+        let paddedvec = hitboxvec + padvec;
+        drop(hitboxvec);
+        drop(padvec);
+        (point.x >= paddedvec.extract(0)) && (point.x <= paddedvec.extract(1)) &&
+        (point.y >= paddedvec.extract(2)) && (point.y <= paddedvec.extract(3))
     }
 }
 
--- src/rust_functions.rkt
+++ src/rust_functions.rkt
@@ -1,6 +1,6 @@
 #lang racket/base
 
-(provide get_country)
+(provide get_board_padding get_country)
 
 (require ffi/unsafe
          ffi/unsafe/define
@@ -11,4 +11,10 @@
 (define libpath (string-append (path->string cd) libname))
 (define-ffi-definer define-rust (ffi-lib libpath))
 
-(define-rust get_country (_fun _int _int -> _string))
+(define-rust get_x_pad (_fun _int -> _int))
+(define-rust get_y_pad (_fun _int -> _int))
+(define (get_board_padding screenwidth screenheight)
+  (values (get_x_pad screenwidth) (get_y_pad screenheight)))
+
+; (get_country mousex mousey board_x_pad board_y_pad) -> country name
+(define-rust get_country (_fun _int _int _int _int -> _string))
